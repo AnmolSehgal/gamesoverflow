b2587a6ff203d47ecfcfcd533de15c22f16c962a
[Anmol Sehgal] <[idx2882@gmail.com]>
2021-05-09 22:26:20 +0530

    adding index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+const  App = () =>{
+	return(
+		<div>Hello There</div>
+	)
+}
+
+ReactDom.render(<App/>,document.getElementByID("root"));
