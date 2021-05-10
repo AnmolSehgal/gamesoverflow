0f0fdc6a7513b3508bfefe812bea9816e43e2868
sourabhsaini11 <ss1841620@gmail.com>
2021-05-10 21:51:14 +0530

    navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//components
+import Nav from "./components/Nav";
+function App() {
+
+  return (
+    <div className="App">
+            <Nav />
+    </div>
+  );
+}
+
+
+export default App;
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-const  App = () =>{
-	return(
-		<div>Hello There</div>
-	)
-}
-
-ReactDom.render(<App/>,document.getElementByID("root"));
