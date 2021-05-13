2ae8a2d772125f6717ed242f1be81bb8bf364efb
[Anmol Sehgal] <[idx2882@gmail.com]>
2021-05-13 21:54:33 +0530

    adding css

--- src/objects/slideshowContent.js
+++
@@ -1,7 +0,0 @@
-const slideShowObj = [
-    {imgSrc:"src/images/cyberpunk_2077_4k-wallpaper-1440x900.jpeg",description:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"},
-    {imgSrc:"src/images/cyberpunk_2077_4k-wallpaper-1440x900.jpeg",description:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"},
-    {imgSrc:"src/images/cyberpunk_2077_4k-wallpaper-1440x900.jpeg",description:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"},
-    {imgSrc:"src/images/cyberpunk_2077_4k-wallpaper-1440x900.jpeg",description:"Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"},
-                    ]
-export default slideShowObj;
