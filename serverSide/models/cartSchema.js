21945d472b1cb5165b8bfcd75c78fb2bc8d655bd
[Anmol Sehgal] <[idx2882@gmail.com]>
2021-05-30 12:11:10 +0530

    adding cart and order schema

--- serverSide/models/cartSchema.js
+++ serverSide/models/cartSchema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
-    userId: {
+    email : {
         type: String,
     },
     items: [{
---
+++ serverSide/models/order.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    email : {
+        type: String,
+    },
+    items: [{
+        productId: {
+            type: String,
+        },
+        name: String,
+        quantity: {
+            type: Number,
+            required: true,
+            min: [1, 'Quantity can not be less then 1.']
+        },
+        price: Number
+    }],
+    bill: {
+        type: Number,
+        required: true
+    },
+    date_added: {
+        type: Date,
+        default: Date.now
+    }
+
+},{timestamp:true});
+
+module.exports = mongoose.model('product',orderSchema);
