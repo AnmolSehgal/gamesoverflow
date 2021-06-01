f3a407df628dd91527b65f7f7c57f98623934a80
[Anmol Sehgal] <[idx2882@gmail.com]>
2021-06-02 02:41:13 +0530

    refershing changes

--- serverSide/middlewares/authentication.js
+++ serverSide/middlewares/authentication.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 
 module.exports.auth = function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
-    console.log(authHeader);
-    const token = authHeader && authHeader.split(' ')[1]
-
+    
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log(token);
     if (token == null) return res.sendStatus(401)
 
     jwt.verify(token, process.env.TOKEN_SECRET, (err,user) => {
